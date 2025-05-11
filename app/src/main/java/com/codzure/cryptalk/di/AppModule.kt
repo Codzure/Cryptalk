dfc7586b52bdece6ce7ae3339d2016e137861016
Leonard Mutugi <leonard.mutugi@equitybank.co.ke>
2025-05-11 20:12:02 +0300

    Onboarding Journey

--- app/src/main/java/com/codzure/cryptalk/di/AppModule.kt
+++ app/src/main/java/com/codzure/cryptalk/di/AppModule.kt
@@ -1,9 +1,12 @@
 package com.codzure.cryptalk.di
 
+import com.codzure.cryptalk.auth.AuthViewModel
+import com.codzure.cryptalk.auth.UserRepository
 import com.codzure.cryptalk.data.MessageRepository
 import com.codzure.cryptalk.data.MessageRepositoryImpl
 import com.codzure.cryptalk.viewmodels.ChatViewModel
 import com.codzure.cryptalk.viewmodels.ChatsListViewModel
+import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
@@ -13,8 +16,10 @@ import org.koin.dsl.module
 val appModule = module {
     // Repositories
     single<MessageRepository> { MessageRepositoryImpl() }
+    single { UserRepository(androidContext()) }
     
     // ViewModels
     viewModel { ChatsListViewModel(get()) }
     viewModel { ChatViewModel(get()) }
+    viewModel { AuthViewModel(get()) }
 }
